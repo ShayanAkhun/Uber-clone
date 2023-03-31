72f0252ed964d9a6c77fa6d37d5542963f6cd8b6
ShayanAkhun <littelmaster391@gmail.com>
2023-03-31 18:05:30 +0500

    added stack navigator

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+P;
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { store } from "./src/Redux/Store/Store";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/Screens/HomeScreen/HomeScreen";
+import MapScreen from "./src/Screens/MapScreen/MapScreen";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
-          <StatusBar style="auto" />
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="MapsScreen" component={MapScreen} />
+            <StatusBar style="auto" />
+          </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
