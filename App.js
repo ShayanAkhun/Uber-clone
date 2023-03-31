05f80d3402edd448dbe48e159e622bf93715e99e
ShayanAkhun <littelmaster391@gmail.com>
2023-03-31 18:06:43 +0500

    removed P

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
-P;
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { store } from "./src/Redux/Store/Store";
 import { NavigationContainer } from "@react-navigation/native";
